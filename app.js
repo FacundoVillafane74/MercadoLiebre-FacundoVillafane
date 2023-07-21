ea44b3ed58d760d50d5d085f14d00cb3e2abb9e4
Facundo Villafañe <facundoagustinvillafane@gmail.com>
2023-07-21 00:28:30 -0300

    agregando link al logo

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv').config();
+const mainRouter = require('./routes/mainRouter');
+const userRouter = require('./routes/userRouter');
 
 const app = express();
 
 const rutaPublic = path.join(__dirname, './public');
 app.use(express.static(rutaPublic));
 
-app.get('/', (req, res) => {
-    const ruta = path.join(__dirname, './views/home.html');
-    res.sendFile(ruta);
-});
+app.use('/', mainRouter);
+app.use('/user', userRouter);
 
-app.get('/login', (req, res) => {
-    const ruta = path.join(__dirname, './views/login.html');
-    res.sendFile(ruta);
-});
 
-app.get('/register', (req, res) => {
-    const ruta = path.join(__dirname, './views/register.html');
-    res.sendFile(ruta);
-});
 
 app.listen(process.env.PORT, () => {
     console.log('servidor escuchando en puerto ' + process.env.PORT + ' - http://localhost:' + process.env.PORT);
